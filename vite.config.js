f85c2672a8c242258379c7baf068c136d4b4c17c
colinpace1987 <colinpace1987@gmail.com>
2026-03-25 12:44:24 -0500

    Fix GH Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/TableTone/",
+  base: "/TableToneClient/",
   server: {
     port: 5173,
   },
